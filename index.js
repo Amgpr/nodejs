b58acf460d11fbe067d6d9b9f555969b7de63e78
Ubuntu <ubuntu@ip-172-31-25-132.ap-southeast-2.compute.internal>
2025-12-16 12:35:00 +0000

    fifth

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();//Respond with "hello world" for requests that hit our root 
 
 app.get('/', function (req, res) { 
 
- res.send('This is a test application '); 
+ res.send('This is a Dockerhub repository '); 
 
 });//listen to port 3000 by default 
 
